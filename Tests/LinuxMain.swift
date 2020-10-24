ff78c3717590ae3a068bcae33dc0287c81a97ca6
John Garrett <jacgarrett18@gmail.com>
2020-10-24 17:16:46 -0400

    clean up

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import new_tab_lyricsTests
-
-var tests = [XCTestCaseEntry]()
-tests += new_tab_lyricsTests.allTests()
-XCTMain(tests)
--- Tests/new-tab-lyricsTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(new_tab_lyricsTests.allTests),
-    ]
-}
-#endif
